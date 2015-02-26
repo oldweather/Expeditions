ef3d5a0943e8de7ed0131c4ccce3116efe566b21
Philip Brohan <philip@brohan.org>
2015-02-26 15:51:26 +0000

    Make video diagnostics.

---
+++ voyages/endurance_1914-6/scripts/expedition_video.R
@@ -0,0 +1,203 @@
+# Show the Route of the ships along with a Reanalysis comparison
+
+library(GSDF.WeatherMap)
+library(chron)
+library(GSDF)
+library(GSDF.TWCR)
+library(parallel)
+library(IMMA)
+
+Endurance<-read.table('Endurance.comparisons')
+Endurance.imma<-IMMA.read('../../../imma/Endurance_1914-16.imma')
+Endurance$Dates<-chron(dates=sprintf("%04d/%02d/%02d",Endurance$V1,
+                          Endurance$V2,Endurance$V3),
+                    times=sprintf("%02d:00:00",Endurance$V4),
+                       format=c(dates='y/m/d',times='h:m:s'))
+
+Aurora<-read.table('Aurora.comparisons')
+Aurora.imma<-IMMA.read('../../../imma/Aurora_1914-16.imma')
+Aurora$Dates<-chron(dates=sprintf("%04d/%02d/%02d",Aurora$V1,
+                          Aurora$V2,Aurora$V3),
+                    times=sprintf("%02d:00:00",Aurora$V4),
+                       format=c(dates='y/m/d',times='h:m:s'))
+
+lDates<-c(Endurance$Dates,Aurora$Dates)
+tics=pretty(lDates)
+ticl=attr(tics,'labels')
+
+GSDF.cache.dir<-sprintf("%s/GSDF.cache",Sys.getenv('GSCRATCH'))
+if(!file.exists(GSDF.cache.dir)) dir.create(GSDF.cache.dir,recursive=TRUE)
+Imagedir<-sprintf("%s/images/ITAE/",Sys.getenv('GSCRATCH'))
+if(!file.exists(Imagedir)) dir.create(Imagedir,recursive=TRUE)
+
+Options<-WeatherMap.set.option(NULL)
+Options<-WeatherMap.set.option(Options,'show.mslp',F)
+Options<-WeatherMap.set.option(Options,'show.ice',T)
+Options<-WeatherMap.set.option(Options,'show.obs',T)
+Options<-WeatherMap.set.option(Options,'show.fog',F)
+Options<-WeatherMap.set.option(Options,'show.wind',F)
+Options<-WeatherMap.set.option(Options,'show.temperature',F)
+Options<-WeatherMap.set.option(Options,'show.precipitation',F)
+Options<-WeatherMap.set.option(Options,'temperature.range',12)
+Options<-WeatherMap.set.option(Options,'obs.size',2.5)
+Options<-WeatherMap.set.option(Options,'obs.colour',rgb(255,215,0,255,
+                                                       maxColorValue=255))
+Options<-WeatherMap.set.option(Options,'lat.min',-55)
+Options<-WeatherMap.set.option(Options,'lat.max',55)
+Options<-WeatherMap.set.option(Options,'lon.min',-98)
+Options<-WeatherMap.set.option(Options,'lon.max',98)
+Options<-WeatherMap.set.option(Options,'pole.lon',180+49)
+Options<-WeatherMap.set.option(Options,'pole.lat',50)
+Options$ice.points<-50000
+
+set.pole<-function(Date,Options) {
+  start.date<-chron(dates='1914/08/08',
+                    times="01:00:00",
+                    format=c(dates='y/m/d',times='h:m:s'))
+  fix.date<-chron(dates='1915/01/01',
+                    times="01:00:00",
+                    format=c(dates='y/m/d',times='h:m:s'))
+  if(Date<=start.date) {
+    Options<-WeatherMap.set.option(Options,'pole.lon',180)
+    Options<-WeatherMap.set.option(Options,'pole.lat',50)
+  }
+  if(Date>=fix.date) {
+    Options<-WeatherMap.set.option(Options,'pole.lon',110)
+    Options<-WeatherMap.set.option(Options,'pole.lat',-1)
+  }
+  if(Date>start.date & Date<fix.date) {
+    Options<-WeatherMap.set.option(Options,'pole.lon',
+                      180+49-70*(Date-start.date)/(fix.date-start.date))
+    Options<-WeatherMap.set.option(Options,'pole.lat',
+                      50-51*(Date-start.date)/(fix.date-start.date))    
+  }
+  return(Options)
+}
+  
+# Make the selected plot
+
+plot.time<-function(c.date) {
+
+   year=as.numeric(as.character(years(c.date)))
+   month=months(c.date)
+   day=days(c.date)
+   hour=hours(c.date)
+   
+    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+    ifile.name<-sprintf("%s/%s",Imagedir,image.name)
+    if(file.exists(ifile.name) && file.info(ifile.name)$size>1000) return()
+    print(sprintf("%04d-%02d-%02d:%02d",year,month,day,hour))
+
+    Options.local<-set.pole(c.date,Options)
+    icec<-TWCR.get.slice.at.hour('icec',year,month,day,hour,
+                                 version='3.5.1')
+    
+    png(ifile.name,
+                 width=1080*WeatherMap.aspect(Options),
+                 height=1080,
+                 bg=Options$sea.colour,
+                 pointsize=24,
+                 type='cairo')
+      Options$label<-sprintf("%04d-%02d-%02d",year,month,day)
+
+      base.gp<-gpar(family='Helvetica',font=1,col='black')
+      pushViewport(dataViewport(c(Options$lon.min,Options$lon.max),
+                                c(Options$lat.min,Options$lat.max),
+                                extension=0,gp=base.gp))
+
+      ip<-WeatherMap.rectpoints(Options$ice.points,Options)
+      WeatherMap.draw.ice(ip$lat,ip$lon,icec,Options.local)
+      WeatherMap.draw.land(NULL,Options.local)
+
+      w<-which(Endurance$Dates<c.date)  
+      if(length(w)>0) {
+          Options.local<-WeatherMap.set.option(Options.local,'obs.colour',
+                                   rgb(200,200,200,255,maxColorValue=255))
+          ot<-list(Longitude=Endurance.imma$LON[w],
+                   Latitude=Endurance.imma$LAT[w])
+          WeatherMap.draw.obs(ot,Options.local)
+     }
+      w<-which(abs(Endurance$Dates-c.date)<1)
+      if(length(w)>0) {
+          Options.local<-WeatherMap.set.option(Options.local,'obs.colour',
+                                   rgb(255,0,0,255,maxColorValue=255))
+          ot<-list(Longitude=Endurance.imma$LON[w],
+                   Latitude=Endurance.imma$LAT[w])
+          WeatherMap.draw.obs(ot,Options.local)
+     }
+      w<-which(Aurora$Dates<c.date)  
+      if(length(w)>0) {
+          Options.local<-WeatherMap.set.option(Options.local,'obs.colour',
+                                   rgb(200,200,200,255,maxColorValue=255))
+          ot<-list(Longitude=Aurora.imma$LON[w],
+                   Latitude=Aurora.imma$LAT[w])
+          WeatherMap.draw.obs(ot,Options.local)
+     }
+      w<-which(abs(Aurora$Dates-c.date)<1)
+      if(length(w)>0) {
+          Options.local<-WeatherMap.set.option(Options.local,'obs.colour',
+                                   rgb(0,0,255,255,maxColorValue=255))
+          ot<-list(Longitude=Aurora.imma$LON[w],
+                   Latitude=Aurora.imma$LAT[w])
+          WeatherMap.draw.obs(ot,Options.local)
+     }
+      if(Options$label != '') {
+            WeatherMap.draw.label(Options)
+      }
+
+   # Add the data plots
+    pushViewport(viewport(width=0.5,height=1.0,x=0.5,y=0.0,
+                          just=c("left","bottom"),name="Page",clip='off'))
+
+    w<-which(Endurance$Dates<=c.date)  
+   # Endurance pressure
+    pushViewport(viewport(width=1.0,height=0.34,x=0.0,y=0.0,
+                          just=c("left","bottom"),name="Page",clip='off'))
+       pushViewport(plotViewport(margins=c(4,6,0,0)))
+          pushViewport(dataViewport(lDates,c(970,1040)))
+         grid.xaxis(at=as.numeric(tics),label=ticl,main=T)
+         grid.text('Date',y=unit(-3,'lines'))
+         grid.yaxis(main=T)
+         grid.text('Sea-level pressure (hPa)',x=unit(-4,'lines'),rot=90)
+         gp=gpar(col=rgb(1,0.8,0.8,1),fill=rgb(1,0.8,0.8,1))
+         for(i in seq_along(Endurance$V1)) {
+            x<-c(Endurance.dates[i]-0.125,Endurance.dates[i]+0.125,
+                 Endurance.dates[i]+0.125,Endurance.dates[i]-0.125)
+            y<-c(Endurance$V6[i]-(Endurance$V7[i])*2,
+                 Endurance$V6[i]-(Endurance$V7[i])*2,
+                 Endurance$V6[i]+(Endurance$V7[i])*2,
+                 Endurance$V6[i]+(Endurance$V7[i])*2)
+            grid.polygon(x=unit(x,'native'),
+                         y=unit(y,'native'),
+                      gp=gp)
+          }
+          popViewport()
+       popViewport()
+    popViewport()
+
+popViewport()
+    
+    dev.off()
+    #gc(verbose=F)
+}
+
+Dates = list()
+count=1
+c.date<-chron(dates="1914/08/08",times="00:00:00",
+          format=c(dates='y/m/d',times='h:m:s'))
+e.date<-chron(dates="1916/03/31",times="23:59:59",
+          format=c(dates='y/m/d',times='h:m:s'))
+
+Dates = list()
+count=1
+while(c.date<e.date) {
+  for(hour in seq(0,23)) {
+     Dates[[count]]<-c.date+hour/24
+     count<-count+1
+   }
+  c.date<-c.date+1
+}
+
+plot.time(Dates[[5000]])
+
+#mclapply(Dates,plot.time,mc.cores=2,mc.preschedule=FALSE)
