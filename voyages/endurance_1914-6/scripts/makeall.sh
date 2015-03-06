895c5d6d07cf95e96b52df14b6019fe59dadf66e
Philip Brohan <philip@brohan.org>
2015-03-06 13:44:25 +0000

    Updated make script to do three vessels.

--- voyages/endurance_1914-6/scripts/makeall.sh
+++ voyages/endurance_1914-6/scripts/makeall.sh
@@ -2,5 +2,6 @@
 
 # Convert to IMMA
 ./Endurance_to_imma.perl < ../as_digitised/ENDURANCE_1914-1915.txt | imma_interpolate.perl > ../../../imma/Endurance_1914-16.imma
-
+./Aurora_to_imma.perl | imma_interpolate.perl > ../../../imma/Aurora_1914-16.imma
+./James_Caird_to_imma.perl < ../as_digitised/JAMES_CAIRD_LOG-APRIL-MAY_1916.txt | imma_interpolate.perl > ../../../imma/James_Caird_1916.imma
 
