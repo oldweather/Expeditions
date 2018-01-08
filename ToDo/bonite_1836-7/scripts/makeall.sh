d13754c16178750b5bf64f1915847a30f27542b3
Philip Brohan <philip@brohan.org>
2018-01-08 17:35:44 +0000

    Updated the Bonite conversion and QC scripts to latest style.

--- ToDo/bonite_1836-7/scripts/makeall.sh
+++ ToDo/bonite_1836-7/scripts/makeall.sh
@@ -3,16 +3,11 @@
 # Convert to IMMA
 ./bonite_to_imma.perl < ../as_digitised/VOYAGE_AUTOUR_DU_MONDE_BONITE_1836.txt | imma_interpolate.perl > ../../../imma/Bonite_1836-7.imma
 
-# Make the KML
-imma_to_kml.perl --title="Bonite 1836-7" --at --sst --colourby=at < ../../../imma/Bonite_1836-7.imma > ../../../kml/Bonite_1836-7.kml
+# Get comparison data from 20CR normals
+R --no-save < 20cr_compare_normal.R
 
+# Plot the route
+R --no-save < Route_map.R
 
-# make the diagnostic figures
-../../../../scripts/obs_v_normals.perl < ../../../imma/Bonite_1836-7.imma > ../../../ovn/Bonite_1836-7.ovn
-ln -sf ../../../ovn/Bonite_1836-7.ovn ovn.out
-
-R --no-save < ../../../utilities/plot_voyage.R > /dev/null
-R --no-save < ../../../utilities/plot_AT.R > /dev/null
-R --no-save < ../../../utilities/plot_SLP.R > /dev/null
-R --no-save < ../../../utilities/plot_SST.R > /dev/null
-
+# Plot the variables
+R --no-save < normals_plot.R
